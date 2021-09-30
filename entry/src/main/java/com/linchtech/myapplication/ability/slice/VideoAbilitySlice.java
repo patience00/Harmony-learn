5f13f2bb23090b84acf15157cccdf4117979a5fd
yaolinqi <523163881@qq.com>
2021-09-30 15:49:28 +0800

    加速播放和快进播放

--- entry/src/main/java/com/linchtech/myapplication/ability/slice/VideoAbilitySlice.java
+++ entry/src/main/java/com/linchtech/myapplication/ability/slice/VideoAbilitySlice.java
@@ -9,10 +9,13 @@ import ohos.agp.components.Image;
 import ohos.agp.components.surfaceprovider.SurfaceProvider;
 import ohos.agp.graphics.Surface;
 import ohos.agp.graphics.SurfaceOps;
+import ohos.agp.window.service.WindowManager;
 import ohos.media.common.Source;
 import ohos.media.player.Player;
+import ohos.multimodalinput.event.MmiPoint;
+import ohos.multimodalinput.event.TouchEvent;
 
-public class VideoAbilitySlice extends AbilitySlice implements Component.ClickedListener, Component.DoubleClickedListener {
+public class VideoAbilitySlice extends AbilitySlice implements Component.TouchEventListener, Component.LongClickedListener, Component.ClickedListener, Component.DoubleClickedListener {
     private Image image;
     // SurfaceProvider对象
     private SurfaceProvider mSurfaceProvider;
@@ -31,14 +34,20 @@ public class VideoAbilitySlice extends AbilitySlice implements Component.Clicked
     public void onStart(Intent intent) {
         super.onStart(intent);
         super.setUIContent(ResourceTable.Layout_ability_video);
+
         mSurfaceProvider = (SurfaceProvider) findComponentById(ResourceTable.Id_surface_provider_video);
         image = (Image) findComponentById(ResourceTable.Id_likeVideo);
         mSurfaceProvider.setClickedListener(this);
         mSurfaceProvider.setDoubleClickedListener(this);
+        mSurfaceProvider.setLongClickedListener(this);
+        mSurfaceProvider.setTouchEventListener(this);
         image.setClickedListener(this);
         image.setDoubleClickedListener(this);
 
-        mSurfaceProvider.pinToZTop(true);
+        // mSurfaceProvider.pinToZTop(true);
+        // 将红心固定在最上面
+        WindowManager.getInstance().getTopWindow().get().setTransparent(true);
+        // mSurfaceProvider.setRotation(180);
         mSurfaceProvider.getSurfaceOps().get().addCallback(new SurfaceOps.Callback() {
             @Override
             public void surfaceCreated(SurfaceOps surfaceOps) {
@@ -74,7 +83,6 @@ public class VideoAbilitySlice extends AbilitySlice implements Component.Clicked
         // stackLayout.moveChildToFront(image);
         // 点击视频页面
         if (component == mSurfaceProvider) {
-            System.out.println("点击播放:" + play);
             try {
                 if (!play) {
                     System.out.println("播放");
@@ -123,13 +131,14 @@ public class VideoAbilitySlice extends AbilitySlice implements Component.Clicked
             // 打开播放视频源文件
             if (mSurfaceProvider.getSurfaceOps().isPresent()) {
                 Surface surface = mSurfaceProvider.getSurfaceOps().get().getSurface();
-                Source source = new Source("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+                // Source source = new Source("http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4");
+                Source source = new Source("http://vfx.mtime.cn/Video/2019/03/14/mp4/190314102306987969.mp4");
                 // 设置音频源
                 mPlayer.setSource(source);
                 mPlayer.setVideoSurface(surface);
                 mPlayer.setPlayerCallback(new VideoPlayerCallback());
                 mPlayer.prepare();
-                mSurfaceProvider.setTop(0);
+                // mSurfaceProvider.setTop(0);
             }
 
         } catch (Exception e) {
@@ -141,10 +150,48 @@ public class VideoAbilitySlice extends AbilitySlice implements Component.Clicked
         // mPlayer.setSurfaceOps(mSurfaceProvider.getSurfaceOps().get());
     }
 
+    @Override
+    public void onLongClicked(Component component) {
+        // 加速播放
+        mPlayer.setPlaybackSpeed(3.0f);
+        System.out.println("三倍速播放");
+    }
+
+    @Override
+    public boolean onTouchEvent(Component component, TouchEvent touchEvent) {
+        int action = touchEvent.getAction();
+        if (action == TouchEvent.PRIMARY_POINT_DOWN) {
+            // 一个手指操作屏幕传0,两个手指则1
+            MmiPoint position = touchEvent.getPointerPosition(0);
+            x = position.getX();
+            y = position.getY();
+        } else if (action == TouchEvent.POINT_MOVE) {
+        } else if (action == TouchEvent.PRIMARY_POINT_UP) {
+            MmiPoint position = touchEvent.getPointerPosition(0);
+            float endX = position.getX();
+            float endY = position.getY();
+            // 左滑动
+            if (endX > x && Math.abs(endY - y) < 100) {
+                System.out.println("快退");
+                mPlayer.rewindTo(-10000L);
+            }
+            // 右滑动
+            if (endX < x && Math.abs(endY - y) < 100) {
+                System.out.println("快进");
+                mPlayer.rewindTo(10000L);
+            }
+
+        }
+        // true,所有的动作都会触发当前方法
+        // false,只有第一个动作会触发当前方法,后续动作就不会触发方法
+        return true;
+
+
+    }
+
     private class VideoPlayerCallback implements Player.IPlayerCallback {
 
         @Override
-
         public void onPrepared() {
             System.out.println("onPrepared");
         }
